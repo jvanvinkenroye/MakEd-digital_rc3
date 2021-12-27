2833e9b8f0be2843abd0e4e4f4b4471da5ee07d2
Jan Vanvinkenroye <jan@vanvinkenroye.de>
2021-12-27 12:00:36 +0100

    Squashed commit of the following:
    commit 1df20c245aae0c08517d708891bc9b4826db8680
    Author: Jan Vanvinkenroye <jan@vanvinkenroye.de>
    Date:   Mon Dec 27 11:57:34 2021 +0100
    
        Updates für rc3_2021
    
        Updates für rc3_2021
    
    commit 54299f7368200410905559392c32e4afbbefefee
    Author: Jan Vanvinkenroye <jan@vanvinkenroye.de>
    Date:   Thu Dec 23 20:13:40 2021 +0100
    
        Update map.json
    
        jitsi durch bbb ersetzt
    
    commit b0ef2f42e2ee6f86cbfa9e191b2e08e0e20505ac
    Author: Jan Vanvinkenroye <jan@vanvinkenroye.de>
    Date:   Thu Dec 23 17:02:00 2021 +0100
    
        Update README.md

---
+++ Tiles/used/cert_safety_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="cert_safety_set" tilewidth="32" tileheight="32" tilecount="32" columns="16">
+ <image source="../MakEd Tiles_von rc3/cert_safety_set.png" width="512" height="64"/>
+</tileset>
---
+++ rc3-mullana-objects Kopie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="rc3-mullana-objects Kopie" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="Tiles/used/rc3-mullana-objects Kopie.png" width="512" height="256"/>
+</tileset>
