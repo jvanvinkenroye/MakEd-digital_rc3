128b1e8ae0cd9060822e96be0e445f269c7e199f
Jan Vanvinkenroye <jan@vanvinkenroye.de>
2021-12-27 15:41:55 +0100

    missing name from tileset

---
+++ AceSet-Hospital.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="AceSet-Hospital" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../MakEd-digital/Tiles/used/AceSet-Hospital.png" width="512" height="512"/>
+ <tile id="52">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name=" collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name=" collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name=" collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name=" collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name=" collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name=" collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="186">
+  <properties>
+   <property name=" collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
